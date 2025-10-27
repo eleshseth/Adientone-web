e824286d98576a39cca16210287b1b6abac6dd5e
elesh seth <elesh.seth@gmail.com>
2025-10-27 11:44:32 +0530

    mobile change

--- frontend/src/components/HowDifferent/HowDifferent.jsx
+++ frontend/src/components/HowDifferent/HowDifferent.jsx
@@ -14,14 +14,18 @@ const HowDifferent = () => {
           />
           <h2 className='different-head'>How We Are Different</h2>
           <p>
-            There is no one-size-fits-all approach to recruitment, there must be a
-            process tailored to the type of business and to the type of people the
-            business wants to recruit.
+            There is no one-size-fits-all approach to recruitment, there must be
+            a process tailored to the type of business and to the type of people
+            the business wants to recruit.
           </p>
           <button className='different-button'>Learn More</button>
         </div>
         <div className='different-image-container'>
-          <img src={assets.diffrent} alt='different approach' className='different-image' />
+          <img
+            src={assets.diffrent}
+            alt='different approach'
+            className='different-image'
+          />
           <div className='different-overlay'></div>
         </div>
       </div>
